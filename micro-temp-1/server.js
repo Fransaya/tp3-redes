075d1a01311c2dc24fed024607560c1d2a595053
fran sayago <fransayago04@gmail.com>
2025-08-30 15:48:19 -0300

    feature: solucion de error micro del brizzio

--- micro-temp-1/server.js
+++ micro-temp-1/server.js
@@ -1,17 +1,26 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import temperatureRoutes from './routes/temperatureRoutes.js';
-import authRoutes from './routes/authRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import temperatureRoutes from "./routes/temperatureRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+
+import http from "http";
 
 dotenv.config();
+
+// Funcion de conexion de ws
+import { connectAndSend } from "./websocket.js";
 
 const app = express();
 const port = 3000;
 
+const server = http.createServer(app);
+
+connectAndSend();
+
 // Rutas
-app.use('/', temperatureRoutes);
-app.use('/', authRoutes);
+app.use("/", temperatureRoutes);
+app.use("/", authRoutes);
 
 app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
