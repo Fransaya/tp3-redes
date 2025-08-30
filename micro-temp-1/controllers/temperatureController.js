80fd3b4f75a6255d7ba1391f5ed2b52a28c0b09a
rumachella <contacto.rumachella@gmail.com>
2025-08-30 14:26:40 -0300

    division en carpetas del server.js

---
+++ micro-temp-1/controllers/temperatureController.js
@@ -0,0 +1,26 @@
+import { v4 as uuidv4 } from 'uuid';
+import { cities, getTemperature } from '../services/temperatureService.js';
+
+export const getTemperatures = async (req, res) => {
+    try {
+        const results = [];
+
+        for (const key of Object.keys(cities)) {
+            const { lat, lon, city } = cities[key];
+            const temperature = await getTemperature(lat, lon);
+             const timestamp = Date.now();
+            results.push({
+                source: "ws-capture-1",
+                city,
+                temperature,
+                captured_at: timestamp,
+                trace_id: uuidv4()
+            });
+        }
+
+        res.json(results);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'No se pudo obtener la temperatura' });
+    }
+};
